48ecef9ac5b8e7d24d60a2c6d982f49b58eeedbf
AndrewManko <a.manko@g.nsu.ru>
2018-12-25 04:06:34 +0700

    by andrew

--- case.py
+++ case.py
@@ -1,6 +1,6 @@
 import random
 
-with open('text.txt') as f_in:
+with open('text.txt', 'r', encoding='ISO-8859-1') as f_in:
     text = f_in.read()
 sym = "+-:;=@#$%^&*()~№<>{}[]"
 
@@ -9,9 +9,9 @@ for i in sym:
         text = text.replace(i, '')
 
 spaces = [' .', ' ,', ' !', ' ?', '  ']
-for i in range (len(spaces)):
+for i in range(len(spaces)):
     if spaces[i] in text:
-        sign=spaces[i]
+        sign = spaces[i]
         text = text.replace(sign, sign[1])
 
 if '\n' in text:
@@ -20,9 +20,22 @@ if '\n' in text:
 words = text.split()
 start = []
 capital = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ"
-for i in range (len(words)):
+for i in range(len(words)):
     word = words[i]
     for char in capital:
         if char in word:
             start.append(word)
-print(start)
+
+dict_1 = {}
+for j in words:
+    lenght = len(j)
+    for i in start:
+        if i in j:
+            list_of_words = []
+            for k in range(1, lenght):
+                list_of_words.append(j[k])
+            dict_1.update({i: list_of_words})
+
+g = len(start)
+e = random.randint(0, g)
+print(start[e], end='')
